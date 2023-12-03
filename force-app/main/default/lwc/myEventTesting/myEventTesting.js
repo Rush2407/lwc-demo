e244d227c709534a2cdd679082b0e93c8398cab6
Rushikesh <rushikeshshelar24@gmail.com>
2023-12-03 15:44:28 +0530

    Test for Data Binding and Event Testing

---
+++ force-app/main/default/lwc/myEventTesting/myEventTesting.js
@@ -0,0 +1,8 @@
+import { LightningElement, track } from 'lwc';
+
+export default class MyEventTesting extends LightningElement {
+    @track greeting = "World";
+    handleChange(event) {
+        this.greeting = event.target.value;
+    }
+}
